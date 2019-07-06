90ff9c833371fb3e6f3fade549985c31a8bf114b
Wen Sun <syl1887415157@126.com>
2019-07-06 10:40:52 +0800

    feat: add an event bus using state

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,33 +1,38 @@
 <template>
   <div class="footer-bar">
     <template v-for="i in localInterval">
-      <br :key="i"/>
+      <span :key="i">{{ i }}<br/></span>
     </template>
+    <span>total from bus: {{ total }}</span>
+    |
     <button @click="expandInterval">expand</button>
   </div>
 </template>
 
 <script>
   import { onMounted, value } from 'vue-function-api'
+  import { bus } from '@/utils/bus'
 
   const FooterBar = {
     props: {
       interval: String
     },
     setup (props, context) {
-      // data
+      // local data, or from bus
       const localInterval = value(Number(props.interval))
+      const total = bus.total
       // mounted
       onMounted(() => {
-        console.log('after mounted: ' + props.interval)
+        console.log('after mounted: interval is ' + props.interval)
       })
       // methods
       const expandInterval = () => {
         localInterval.value++
-        console.log('after expanded: ' + localInterval.value)
+        console.log('after expanded: interval is ' + localInterval.value)
       }
       return {
         localInterval,
+        total,
         expandInterval
       }
     }
