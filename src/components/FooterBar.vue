a9f5b635f2859e75c5f4e4d5164f84b7bc076b12
Wen Sun <syl1887415157@126.com>
2019-07-06 10:04:33 +0800

    feat: add a footer bar

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="footer-bar">
+    <template v-for="i in localInterval">
+      <br :key="i"/>
+    </template>
+    <button @click="expandInterval">expand</button>
+  </div>
+</template>
+
+<script>
+  import { onMounted, value } from 'vue-function-api'
+
+  const FooterBar = {
+    props: {
+      interval: Number
+    },
+    setup (props, context) {
+      // data
+      const localInterval = value(props.interval)
+      // mounted
+      onMounted(() => {
+        console.log('after mounted: ' + props.interval)
+      })
+      // methods
+      const expandInterval = () => {
+        localInterval.value++
+        console.log('after expanded: ' + localInterval.value)
+      }
+      return {
+        localInterval,
+        expandInterval
+      }
+    }
+  }
+  export default FooterBar
+</script>
+
+<style scoped>
+
+</style>
--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -2,15 +2,17 @@
   <div class="wrapper">
     <SearchBar></SearchBar>
     <ListBody></ListBody>
+    <FooterBar :interval="1"></FooterBar>
   </div>
 </template>
 
 <script>
   import SearchBar from '@/components/SearchBar'
   import ListBody from '@/components/ListBody'
+  import FooterBar from '@/components/FooterBar'
 
   const TodoList = {
-    components: { ListBody, SearchBar }
+    components: { ListBody, SearchBar, FooterBar }
   }
 
   export default TodoList
